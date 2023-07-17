ab222d6d89ce876676320b026f6f48877f4eb8fa
xMintix <137613114+xMintix@users.noreply.github.com>
2023-07-17 17:43:27 +0930

    Update MainString.lua

--- MainString.lua
+++ MainString.lua
@@ -1,14 +1,14 @@
-local Drawing=Drawing.new("Text")
-Drawing.Visible=true
-Drawing.Text="Brought to you by @xMintix#0 on discord"
-Drawing.Transparency=0
-Drawing.Font=32
-Drawing.Size=32
-Drawing.Color=Color3.new(255,0,0)
-game:GetService("RunService").RenderStepped:Connect(function()Drawing.Position=Vector2.new(workspace.CurrentCamera.ViewportSize.X/2-100
+local a=Drawing.new("Text")
+a.Visible=true
+a.Text="Brought to you by @xMintix#0 on discord"
+a.Transparency=2
+a.Font=14
+a.Size=33
+a.Color=Color3.new(255,0,0)
+game:GetService("RunService").RenderStepped:Connect(function()a.Position=Vector2.new(workspace.CurrentCamera.ViewportSize.X/2-100
 ,workspace.CurrentCamera.ViewportSize.Y/2)end)
 wait(3)
-Drawing:Remove()
+a:Remove()
 
 
 if game.PlaceId == 2098516465 then --Execute RobloxianHighSchool2 Script If You Join RHS2 
