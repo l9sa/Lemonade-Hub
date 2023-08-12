f285519ee6551a09765c13143d99bce824da5c42
xMintix <137613114+xMintix@users.noreply.github.com>
2023-08-13 01:17:34 +0930

    Update MainString.lua

--- MainString.lua
+++ MainString.lua
@@ -13,10 +13,8 @@ Gui:Remove()
 
 if game.PlaceId == 2098516465 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/xMintix/Lemonade-Hub/main/RHS2String.lua"))()
-    print("RHS2 Script Executed")
 elseif game.PlaceId == 1345139196 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/xMintix/Lemonade-Hub/main/Treasure%20Hunt%20Sim/TreasureHuntString.lua"))()
-    print("Treasure Hunt Simulator Script Executed")
 else
     loadstring(game:HttpGet("https://raw.githubusercontent.com/xMintix/Lemonade-Hub/main/Universal"))()
 end
