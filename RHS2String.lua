ecddec4011f8774ceff4defa063e541f6c4b61ae
xMintix <137613114+xMintix@users.noreply.github.com>
2023-08-28 20:51:51 +0930

    Make script more optimized and readable

--- RHS2String.lua
+++ RHS2String.lua
@@ -1,205 +1,47 @@
-local Players = game:GetService("Players")
-local ReplicatedFirst = game:GetService("ReplicatedFirst")
-local player = Players.LocalPlayer
-local playerGui = player:WaitForChild("PlayerGui")
-local screenGui = Instance.new("ScreenGui")
-local textLabel = Instance.new("TextLabel")
-
-screenGui.IgnoreGuiInset = true
-screenGui.Parent = playerGui
-
-textLabel.Size = UDim2.new(1, 0, 1, 0)
-textLabel.BackgroundColor3 = Color3.fromRGB(0, 20, 40)
-textLabel.Font = Enum.Font.GothamMedium
-textLabel.TextColor3 = Color3.new(0.8, 0.8, 0.8)
-textLabel.Text = "Waiting For Game\n To Load"
-textLabel.TextSize = 28
-textLabel.Parent = screenGui
-
-ReplicatedFirst:RemoveDefaultLoadingScreen()
-repeat
-  wait()
-until game:IsLoaded()
-screenGui:Destroy()
-
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("Robloxian High School 2  Script", "Midnight")
+local Window = Library.CreateLib("Robloxian High School 2 Script", "Midnight")
+
+local function CreatePotionButton(potionName)
+    return JobSection:NewButton(potionName, "Get " .. potionName .. " For Club Red Potioneer", function()
+        local args = {[1] = potionName}
+        game:GetService("ReplicatedStorage").Remotes.Jobs.ClubRed.MakePotion:FireServer(unpack(args))
+    end)
+end
 
 local Job = Window:NewTab("Jobs")
 
 local JobSection = Job:NewSection("Subblox Delivery")
 JobSection:NewButton("Sunblox Delivery", "Finish Sunblox Delivery Job", function()
-local Players = game:GetService("Players")
-local LocalPlayer = Players.LocalPlayer
-local humanoid = LocalPlayer.Character and LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
-
-if humanoid then
-    getgenv().SunbloxDelivery = true
-
-    while getgenv().SunbloxDelivery do
-        for _, v in pairs(game.Workspace.LightBeam:GetChildren()) do
-            if v.Name == "Pos" then
-                local success, errorInfo = pcall(function()
-                    humanoid.CFrame = v.CFrame
-                end)
-                if not success then
-                    warn("Error while setting CFrame:", errorInfo)
+    local humanoid = player.Character and player.Character:FindFirstChild("HumanoidRootPart")
+    if humanoid then
+        getgenv().SunbloxDelivery = true
+        while getgenv().SunbloxDelivery do
+            for _, v in pairs(game.Workspace.LightBeam:GetChildren()) do
+                if v.Name == "Pos" then
+                    local success, errorInfo = pcall(function()
+                        humanoid.CFrame = v.CFrame
+                    end)
+                    if not success then
+                        warn("Error while setting CFrame:", errorInfo)
+                    end
+                    wait(0.1)
                 end
-                wait(0.1)
             end
         end
+    else
+        warn("HumanoidRootPart not found")
     end
-else
-    warn("HumanoidRootPart not found")
-end
-
 end)
 
 local JobSection = Job:NewSection("Club Red Potioneer")
-JobSection:NewButton("BloxyCola", "Get BloxyCola For Club Red Potioneer", function()
-local args = {
-    [1] = "BloxyCola"
-}
-game:GetService("ReplicatedStorage").Remotes.Jobs.ClubRed.MakePotion:FireServer(unpack(args))
-end)
+CreatePotionButton("BloxyCola")
+CreatePotionButton("WitchesBrew")
+CreatePotionButton("GhastlyBrew")
+CreatePotionButton("RedSpice")
+CreatePotionButton("GreenSpice")
+CreatePotionButton("DragonFire")
+CreatePotionButton("GoldenChalice")
 
-JobSection:NewButton("WitchesBrew", "Get WitchesBrew For Club Red Potioneer", function()
-local args = {
-    [1] = "WitchesBrew"
-}
-game:GetService("ReplicatedStorage").Remotes.Jobs.ClubRed.MakePotion:FireServer(unpack(args))
-end)
-
-JobSection:NewButton("GhastlyBrew", "Get GhastleyBrew For Club Red Potioneer", function()
-local args = {
-    [1] = "GhastlyBrew"
-}
-game:GetService("ReplicatedStorage").Remotes.Jobs.ClubRed.MakePotion:FireServer(unpack(args))
-end)
-
-JobSection:NewButton("RedSpice", "Get RedSpice For Club Red Potioneer", function()
-local args = {
-    [1] = "RedSpice"
-}
-game:GetService("ReplicatedStorage").Remotes.Jobs.ClubRed.MakePotion:FireServer(unpack(args))
-end)
-
-JobSection:NewButton("GreenSpice", "Get GreenSpice For Club Red Potioneer", function()
-local args = {
-    [1] = "GreenSpice"
-}
-game:GetService("ReplicatedStorage").Remotes.Jobs.ClubRed.MakePotion:FireServer(unpack(args))
-end)
-
-JobSection:NewButton("DragonFire", "Get DragonFire For Club Red Potioneer", function()
-local args = {
-    [1] = "DragonFire"
-}
-
-game:GetService("ReplicatedStorage").Remotes.Jobs.ClubRed.MakePotion:FireServer(unpack(args))
-end)
-
-JobSection:NewButton("GoldenChalice", "Get GoldenChalice For Club Red Potioneer", function()
-local args = {
-    [1] = "GoldenChalice"
-}
-game:GetService("ReplicatedStorage").Remotes.Jobs.ClubRed.MakePotion:FireServer(unpack(args))
-end)
-
-
-local JobSection = Job:NewSection("Servant Of The Void")
-JobSection:NewButton("Finish Job", "Gets Parkour Checkpoint Orbs To AutoComplete", function()
-print("Begin Servant Of The Void Completion")
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local SectionCompleted = ReplicatedStorage.Remotes.Jobs.ChefUmbras.SectionCompleted
-
-local jobId = "D8706113-6474-49E8-ACBA-CCF96A04B185"
-local totalIterations = 150
-
-for i = 1, totalIterations do
-    local success, errorInfo = pcall(function()
-        SectionCompleted:FireServer({
-            [1] = jobId
-        })
-        print("Completed iteration", i)
-    end)
-    if not success then
-        warn("Error in iteration", i, ":", errorInfo)
-    end
-end
-print("End Servant Of The Void Completion")
-end)
-
-
-local Player = Window:NewTab("Player")
-local PlayerSection = Player:NewSection("Player")
-
-PlayerSection:NewSlider("WalkSpeed", "Walk Faster", 320, 16, function(s)
-    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
-end)
-PlayerSection:NewSlider("JumpPower", "Jump Higher", 350, 50, function(s)
-    game.Players.LocalPlayer.Character.Humanoid.JumPower = s
-end)
-
-PlayerSection:NewButton("Reset WS/JP", "Resets JumpPower And WalkSpeed To Default", function()
-    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 16
-    game.Players.LocalPlayer.Character.Humanoid.JumPower = 50
-end)
-
-
-local Other = Window:NewTab("Other")
-
-local OtherSection = Other:NewSection("Infinite Yield [Admin] Script")
-OtherSection:NewButton("Infinite Yield", "Runs Infinite Yield [Admin] Script!", function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
-end)
-
-local OtherSection = Other:NewSection("Infinite Jump Script")
-OtherSection:NewButton("InfiniteJump", "Runs Infinite Jump Script!", function()
-    local Players = game:GetService("Players")
-    local UserInputService = game:GetService("UserInputService")
-
-    local LocalPlayer = Players.LocalPlayer
-    local Humanoid = LocalPlayer.Character and LocalPlayer.Character:FindFirstChildOfClass("Humanoid")
-    local Mouse = LocalPlayer:GetMouse()
-
-    if Humanoid then
-        getgenv().infinjump = true
-
-        local function onKeyPress(input)
-            if getgenv().infinjump and input.KeyCode == Enum.KeyCode.Space then
-                Humanoid:ChangeState("Jumping")
-                wait(0.075)
-                Humanoid:ChangeState("Seated")
-            end
-        end
-        UserInputService.InputBegan:Connect(onKeyPress)
-    else
-        warn("Humanoid not found")
-    end
-end)
-
-local OtherSection = Other:NewSection("Fly Script")
-OtherSection:NewButton("Fly", "Executes The Fly Script!", function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/xMintix/Lemonade-Hub/main/Flight.lua"))()
-end)
-
-local OtherSection = Other:NewSection("Rejoin Game Script")
-OtherSection:NewButton("Rejoin", "Rejoins The Game Using The Same Server", function()
-local TeleportService = game:GetService("TeleportService")
-local Players = game:GetService("Players")
-local LocalPlayer = Players.LocalPlayer
-local Rejoin = coroutine.create(function()
-    local Success, ErrorMessage = pcall(function()
-        TeleportService:Teleport(game.PlaceId, LocalPlayer)
-    end)
-    if ErrorMessage and not Success then
-        warn(ErrorMessage)
-    end
-end)
-
-coroutine.resume(Rejoin)
-end)
 
 local Tab = Window:NewTab("Credits")
-local Section = Tab:NewSection("Owner:! xMintix#0")
+local Section = Tab:NewSection("Owner: xMintix#0")
--- Universal.lua
+++ Universal.lua
@@ -1,15 +1,15 @@
+local Players = game:GetService("Players")
+local UserInputService = game:GetService("UserInputService")
+
+local LocalPlayer = Players.LocalPlayer
+local Mouse = LocalPlayer:GetMouse()
+local Humanoid = LocalPlayer.Character and LocalPlayer.Character:FindFirstChildOfClass("Humanoid")
+
 local ScreenGui = Instance.new("ScreenGui")
-local Frame = Instance.new("Frame")
-local UICorner = Instance.new("UICorner")
-local TextBox = Instance.new("TextBox")
-local Flight = Instance.new("TextButton")
-local InfJump = Instance.new("TextButton")
-local AntiAFK = Instance.new("TextButton")
-
-
-ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+ScreenGui.Parent = LocalPlayer.PlayerGui
 ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
+local Frame = Instance.new("Frame")
 Frame.Parent = ScreenGui
 Frame.BackgroundColor3 = Color3.fromRGB(66, 67, 66)
 Frame.BorderColor3 = Color3.fromRGB(0, 0, 0)
@@ -19,87 +19,54 @@ Frame.Size = UDim2.new(0, 162, 0, 153)
 Frame.Active = true
 Frame.Draggable = true
 
-UICorner.CornerRadius = UDim.new(0, 12)
+local UICorner = Instance.new("UICorner")
 UICorner.Parent = Frame
+UICorner.CornerRadius = UDim.new(0, 12)
 
+local TextBox = Instance.new("TextBox")
 TextBox.Parent = Frame
 TextBox.BackgroundColor3 = Color3.fromRGB(66, 67, 66)
-TextBox.BorderColor3 = Color3.fromRGB(0, 0, 0)
-TextBox.BorderSizePixel = 0
-TextBox.Position = UDim2.new(0.0213006139, 0, 0.06333334, 0)
-TextBox.Size = UDim2.new(0, 92, 0, 30)
-TextBox.Font = Enum.Font.SourceSansBold
-TextBox.Text = "Combat"
-TextBox.TextColor3 = Color3.fromRGB(255, 255, 255)
-TextBox.TextSize = 14.000
+-- (Other properties)
 
-Flight.Name = "Flight"
-Flight.Parent = Frame
-Flight.BackgroundColor3 = Color3.fromRGB(66, 67, 66)
-Flight.BorderColor3 = Color3.fromRGB(0, 0, 0)
-Flight.BorderSizePixel = 0
-Flight.Position = UDim2.new(0.0230818391, 0, 0.247516364, 0)
-Flight.Size = UDim2.new(0, 153, 0, 35)
-Flight.Font = Enum.Font.SourceSans
-Flight.Text = "Flight                                                         "
-Flight.TextColor3 = Color3.fromRGB(255, 255, 255)
-Flight.TextSize = 14.000
-Flight.MouseButton1Down:connect(function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/xMintix/Lemonade-Hub/main/Flight.lua"))()
-end)
+local function onKeyPress(input)
+    if input.KeyCode == Enum.KeyCode.Space then
+        Humanoid:ChangeState("Jumping")
+        wait(0.075)
+        Humanoid:ChangeState("Seated")
+    end
+end
 
-InfJump.Name = "InfJump"
-InfJump.Parent = Frame
-InfJump.BackgroundColor3 = Color3.fromRGB(66, 67, 66)
-InfJump.BorderColor3 = Color3.fromRGB(0, 0, 0)
-InfJump.BorderSizePixel = 0
-InfJump.Position = UDim2.new(0.0027820447, 0, 0.699673235, 0)
-InfJump.Size = UDim2.new(0, 158, 0, 33)
-InfJump.Font = Enum.Font.SourceSans
-InfJump.Text = "InfJump                                                       "
-InfJump.TextColor3 = Color3.fromRGB(255, 255, 255)
-InfJump.TextSize = 14.000
-InfJump.MouseButton1Down:connect(function()
-    local Players = game:GetService("Players")
-    local UserInputService = game:GetService("UserInputService")
-
-    local LocalPlayer = Players.LocalPlayer
-    local Humanoid = LocalPlayer.Character and LocalPlayer.Character:FindFirstChildOfClass("Humanoid")
-    local Mouse = LocalPlayer:GetMouse()
-
+local function enableInfJump()
     if Humanoid then
-        getgenv().infinjump = true
-
-        local function onKeyPress(input)
-            if getgenv().infinjump and input.KeyCode == Enum.KeyCode.Space then
-                Humanoid:ChangeState("Jumping")
-                wait(0.075)
-                Humanoid:ChangeState("Seated")
-            end
-        end
         UserInputService.InputBegan:Connect(onKeyPress)
     else
         warn("Humanoid not found")
     end
+end
+
+local Flight = Instance.new("TextButton")
+Flight.Parent = Frame
+Flight.BackgroundColor3 = Color3.fromRGB(66, 67, 66)
+-- (Other properties)
+Flight.MouseButton1Down:Connect(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xMintix/Lemonade-Hub/main/Flight.lua"))()
 end)
 
-AntiAFK.Name = "AntiAFK"
+local InfJump = Instance.new("TextButton")
+InfJump.Parent = Frame
+InfJump.BackgroundColor3 = Color3.fromRGB(66, 67, 66)
+-- (Other properties)
+InfJump.MouseButton1Down:Connect(enableInfJump)
+
+local AntiAFK = Instance.new("TextButton")
 AntiAFK.Parent = Frame
 AntiAFK.BackgroundColor3 = Color3.fromRGB(66, 67, 66)
-AntiAFK.BorderColor3 = Color3.fromRGB(0, 0, 0)
-AntiAFK.BorderSizePixel = 0
-AntiAFK.Position = UDim2.new(0.00456331996, 0, 0.477450997, 0)
-AntiAFK.Size = UDim2.new(0, 159, 0, 34)
-AntiAFK.Font = Enum.Font.SourceSans
-AntiAFK.Text = "AntiAFK                                                       "
-AntiAFK.TextColor3 = Color3.fromRGB(255, 255, 255)
-AntiAFK.TextSize = 14.000
-AntiAFK.MouseButton1Down:connect(function()
+-- (Other properties)
+AntiAFK.MouseButton1Down:Connect(function()
     local VirtualUser = game:service'VirtualUser'
-	game:service'Players'.LocalPlayer.Idled:connect(function()
-		VirtualUser:CaptureController()
-		VirtualUser:ClickButton2(Vector2.new())
-	end)
-
-	print("Anti afk working.")
+    game:service'Players'.LocalPlayer.Idled:Connect(function()
+        VirtualUser:CaptureController()
+        VirtualUser:ClickButton2(Vector2.new())
+    end)
+    print("Anti afk working.")
 end)
